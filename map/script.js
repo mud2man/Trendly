2c909ba63812560f5efaad1f8f4ca912bad3c263
Namo.Lu <chih-hung.lu@columbia.edu>
2017-12-07 22:06:30 -0500

    Add text display on the marker

--- map/script.js
+++ map/script.js
@@ -3,6 +3,28 @@ var globalPositiveLocations = [];
 var globalNegativeLocations = [];
 var usaCenetr = {lat: 40, lng: -98};
 
+function createMarker(location, map, tweet, icon) {
+  var marker = new google.maps.Marker({       
+      position: location, 
+      icon: icon,
+      map: map
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: tweet,
+    maxWidth: 200
+  });
+
+  google.maps.event.addListener(marker, 'mouseover', function() { 
+     infowindow.open(map, marker);
+  }); 
+
+  google.maps.event.addListener(marker, 'mouseout', function() {
+    infowindow.close();
+});
+  return marker;  
+}
+
 function createMap(localPositiveLocations, localNegativeLocations) {
   console.log("Hello createMap");
   map = new google.maps.Map(document.getElementById('map'), {
@@ -13,22 +35,12 @@ function createMap(localPositiveLocations, localNegativeLocations) {
   var markers = [];
   /* put positive marker */
   for(var i = 0; i < localPositiveLocations.length; ++i){
-    var marker = new google.maps.Marker({
-      position: localPositiveLocations[i],
-      icon: 'greenMarker.png',
-      map: map
-    });
-    markers.push();
+    markers.push(createMarker(localPositiveLocations[i].location, map, localPositiveLocations[i].tweet, 'greenMarker.png'));
   }
 
   /* put negative marker */
   for(var i = 0; i < localNegativeLocations.length; ++i){
-    var marker = new google.maps.Marker({
-      position: localNegativeLocations[i],
-      icon: 'redMarker.png',
-      map: map
-    });
-    markers.push();
+    markers.push(createMarker(localNegativeLocations[i].location, map, localNegativeLocations[i].tweet, 'redMarker.png'));
   }
 }
 
@@ -47,5 +59,6 @@ function loadGoogleMap() {
 
 $(document).ready(function() {
   console.log("Hello ready....");
-  drawTrenMap([{lat: 40.807, lng: -73.963}, {lat: 39.807, lng: -73.963}], [{lat: 34.022, lng: -118.285}, {lat: 35.022, lng: -118.285}]);
+  drawTrenMap([{location: {lat: 40.807, lng: -73.963}, tweet: "tweet0"}, {location: {lat: 39.807, lng: -73.963}, tweet: "tweet1"}], 
+              [{location: {lat: 34.022, lng: -118.285}, tweet: "tweet2"}, {location: {lat: 35.022, lng: -118.285}, tweet: "tweet3"}]);
 });
